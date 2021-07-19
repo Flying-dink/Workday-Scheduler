03e4206e9013fca7f78c93062de4f6b12f06f0ba
gail enstice <skyedoll@aol.com>
2021-07-19 17:20:45 -0400

    Worked on Clock

--- script.js
+++ script.js
@@ -2,38 +2,39 @@ function renderClock() {
 
 
 //Day of the week
-var weekday = moment();
-console.log(
-    "Today is :"+
-    weekday.format('dddd')
+var weekday = moment().format('dddd')
+console.log(weekday)
+// console.log(
+//     "Today is :"+
+//     weekday.format('dddd')
     
-);
+// );
 
 
 //The Date
-var today = moment();
-console.log(
+var today = moment().format('DD-MM-YYYY')
+// console.log(
 
-    "Today's date is : "+
-    today.format('DD-MM-YYYY')
-);
+//     "Today's date is : "+
+//     today.format('DD-MM-YYYY')
+// );
 
 //Current Time Display
 
-var now= moment();
-let timeBlock = moment().toDate().getTime()
-console.log( 
-    "The Current Time is : "+
-    now.format('hh: mm')
-);
+var now= moment().format('hh : mm')
+// let timeBlock = moment().toDate().getTime()
+// console.log( 
+//     "The Current Time is : "+
+//     now.format('hh: mm')
+// );
 
 
 //Link or append to HTML ID to display time
 
- var Clock = document.createElement("clock");
- Clock.textContent = "" +weekday+ "" +today+ "" +now+ "|"
- Clock.innerText = "" |
- clock.append
+ var Clock = document.getElementById("currentDay");
+ Clock.textContent = "  " +weekday+ ".  " +today+ ".  " +now+ "  "
+ 
+ 
  
 
 setTimeout('renderclock',1000);
@@ -42,8 +43,7 @@ renderClock();
 
 //Start Timeblocks section
 
-var conatainer
-$('#container'). Container({})
+
 
 
 
@@ -52,13 +52,35 @@ $('#container'). Container({})
 
 
 
-
+function colorBlocks(){
+    $('textarea').each(function(){
+    var blockHour= this.id
+    var currentHour=moment().hours()
+    if (blockHour< currentHour){
+        $(this).addClass('past')
+    }else if (blockHour==currentHour){
+        $(this).removeClass('past')
+        $(this).addClass('present')
+    }else {
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+        $(this).addClass('future')
+    }
+    })
+}
 
 
 
 //Local Storage 
-$('#test').textarea("test");
-localStorage.content =$('#test').textarea();
-$('#test').textarea(localStorage.content);
+
+//localStorage.setItem('textarea');
+//console.log(localStorage.getItem('textarea'));
+//localStorage.removeItem('textarea');
+//console.log(localStorage.getItem(textarea));
+
+// $('#textarea').currentHour("textarea");
+//localStorage.content =$('#textarea').currentHour();
+// $('#textarea').currentHour(localStorage.content);
 
 
+colorBlocks()
